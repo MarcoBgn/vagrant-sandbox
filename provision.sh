62d14daab917288f46b40430bf3544f89cbe854e
Marco Bagnasco <marcobagnasco@ymail.com>
2018-12-17 14:37:13 +0000

    Copy nginx development config into the box
    - remove useless file

--- provision.sh
+++ provision.sh
@@ -2,9 +2,11 @@ apt-get -y update
 
 apt-get -y install nginx
 
+# Remove default box config and copy the local one
 rm -rf /var/www/html
-ln -s /vagrant/html /var/www/html
+cp -r /vagrant/sites-enabled /etc/nginx
 
+# Remove the default html page and create a symlink to the local one
 rm -rf /etc/nginx/sites-enabled
 ln -s /vagrant/sites-enabled
 
