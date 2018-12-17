640a4268d608e5683f1d69a5a4cc24b61ea5d199
Marco Bagnasco <marcobagnasco@ymail.com>
2018-12-17 14:24:49 +0000

    Create symlink to local nginx config

--- provision.sh
+++ provision.sh
@@ -2,8 +2,11 @@ apt-get -y update
 
 apt-get -y install nginx
 
-sudo rm -rf /var/www/html
-sudo ln -s /vagrant/html /var/www/html
+rm -rf /var/www/html
+ln -s /vagrant/html /var/www/html
+
+rm -rf /etc/nginx/sites-enabled
+ln -s /vagrant/sites-enabled
 
 service nginx start
 
