1b8de82863ab2045b1bfd6ed3e767dd538e018bb
Marco Bagnasco <marcobagnasco@ymail.com>
2018-12-18 09:59:38 +0000

    Use ruby-box - share folder in /app

--- provision.sh
+++ provision.sh
@@ -3,12 +3,9 @@ apt-get -y update
 apt-get -y install nginx
 
 # Remove default box config and copy the local one
-rm -rf /var/www/html
-cp -r /vagrant/sites-enabled /etc/nginx
 
-# Remove the default html page and create a symlink to the local one
-rm -rf /etc/nginx/sites-enabled
-ln -s /vagrant/sites-enabled
+# TODO:
+# rm /etc/nginx/sites-available/default
+# cp /vagrant/config/vhost /etc/nginx/sites-available/default
 
 service nginx start
-
